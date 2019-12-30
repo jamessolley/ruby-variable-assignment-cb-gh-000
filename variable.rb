c029a771be8f253f3e644205d6dbf329e1a2c640
Joey Lopez <js1102923@swccd.edu>
2019-12-30 03:58:48 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,2 +1,2 @@
-var greeting = "Hello World" # Define a local variable called 'greeting',
+var greeting = "Hello World"; # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
